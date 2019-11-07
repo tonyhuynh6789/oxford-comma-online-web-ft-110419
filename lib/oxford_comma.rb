c08b2e9ac21152561993f52cbadfefeca19d6e6d
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-07 02:32:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+
 def oxford_comma(array)
 
+if array.length == 1
+    array.join
+elsif array.length == 2
+    array[0] << " and "
+    array.join
+elsif array.length > 2
+    array[-1].prepend "and "
+    array.join(", ")
 end
+end 
